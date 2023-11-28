889f46cbc70179e5edf2de43986a9c8f26ecac3b
Dmitry Kozak <dimakaza4ka@gmail.com>
2023-11-28 08:37:46 +0300

    Added GameRunner

---
+++ Assets/Scripts/Infostructure/GameRunner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+namespace Scripts.Infostructure
+{
+    public class GameRunner : MonoBehaviour
+    {
+        public GameBootstrapper bootstrapperPrefab;
+        private void Awake()
+        {
+            var bootstrapper = FindObjectOfType<GameBootstrapper>();
+            if (bootstrapper == null)
+            {
+                Instantiate(bootstrapperPrefab);
+            }
+        }
+    }
+}
