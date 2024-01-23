8c6a2f44dba0ab171cc04a5403e8b7d5d3bdd916
Dmitry Kozak <dimakaza4ka@gmail.com>
2024-01-23 11:54:39 +0300

    Added difficulty StaticData

---
+++ Assets/Scripts/StaticData/DifficultyPreset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scripts.StaticData
+{
+    [CreateAssetMenu(fileName = "DifficultyPreset", menuName = "StaticData/DifficultyData")]
+    public class DifficultyPreset : ScriptableObject
+    {
+        public int DifficultyUpdateTimer;
+        public int Difficulty;
+        public float HpMultiplier;
+        public float SpeedMultiplier;
+    }
+}
