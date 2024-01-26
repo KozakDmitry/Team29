f514344164e55e9d06dafafbf5aab438651b801f
Dmitry Kozak <dimakaza4ka@gmail.com>
2024-01-26 08:46:53 +0300

    Added base prefabs

---
+++ Assets/Scripts/UI/Windows/Menu/Menu.cs
@@ -0,0 +1,22 @@
+﻿
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.UI.Windows.Menu
+{
+    public class Menu : WindowBase
+    {
+        public Button ExitButton;
+        protected override void Initialize()
+        {
+            ExitButton.onClick.AddListener(()=> Application.Quit());   
+        }
+
+        protected override void CleanUp()
+        {
+            base.CleanUp();
+
+        }
+    }
+}
